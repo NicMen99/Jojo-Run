f1445f5c683f1f34d1294cc4482d2df2b287e60e
angiologiandonati <angiolo.giandonati@stud.unifi.it>
2020-08-18 18:07:49 +0200

    Aggiunto principio di definizione per Hero

--- Src/Hero.h
+++ Src/Hero.h
@@ -5,6 +5,16 @@
 #ifndef JOJO_RUN_HERO_H
 #define JOJO_RUN_HERO_H
 
-//WWWWWWRRRRRRRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy
+#include "Subject.h"
+#include <SFML/Graphics.hpp>
+
+class Hero : public sf::Sprite, public Subject{
+public:
+    //domani continuo ad aggiungere roba
+private:
+    void death() { isDead = true};
+    int hp = 300;
+    bool isDead = false;
+};
 
 #endif //JOJO_RUN_HERO_H
