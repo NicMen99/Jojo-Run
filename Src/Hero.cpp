b25892ab0c3e04c6d215a5312b75fd8d8a03c8e6
angiologiandonati <angiolo.giandonati@stud.unifi.it>
2020-08-19 14:31:43 +0200

    Aggiunti metodi alla classe Hero

--- Src/Hero.cpp
+++ Src/Hero.cpp
@@ -2,3 +2,48 @@
 // Created by angiolo99 on 23/01/20.
 //
 
+#include "Hero.h"
+
+bool Hero::gameOver() {
+    if (hp <= 0)
+        death();
+    return isDead;
+}
+
+Hero::Hero() {
+    hero.setPosition(65,100);
+}
+
+void Hero::setHP(unsigned int hp) {
+    Hero::hp = hp;
+}
+
+void Hero::setHeroTexture(const sf::Texture &heroTexture){
+    hero.setTexture(heroTexture);
+    hero.setScale(0.20, 0.20);
+}
+
+sf::Vector2f Hero::getHeroSize() const {
+    float x = hero.getGlobalBounds().width;
+    float y = hero.getGlobalBounds().height;
+    return sf::Vector2f();
+}
+
+int Hero::getKnives() const {
+    return knives;
+}
+
+void Hero::setKnives(int knives) {
+    Hero::knives = knives;
+}
+
+void Hero::throwKnife() {
+      //da definire
+}
+
+void Hero::renderHero(sf::RenderWindow &map) {
+    map.draw(hero);
+}
+
+
+
--- Src/Hero.h
+++ Src/Hero.h
@@ -11,10 +11,26 @@
 class Hero : public sf::Sprite, public Subject{
 public:
     //domani continuo ad aggiungere roba
+    Hero ();
+    ~Hero() = default;
+    bool gameOver();
+    void setHeroTexture(const sf::Texture &heroTexture);
+    void setHeroPosition(float x, float y) {hero.setPosition(x,y);}
+    void setHP(unsigned int hp);
+    bool getIsDead() const {return isDead;}
+    void throwKnife();
+    sf::Vector2f getHeroSize() const;
+    sf::FloatRect getHeroBounds() const { return hero.getGlobalBounds(); }
+    sf::Vector2f getHeroPos() const { return hero.getPosition(); }
+    int getKnives() const;
+    void setKnives(int knives);
+    void renderHero(sf::RenderWindow &map);
 private:
-    void death() { isDead = true};
+    void death() { isDead = true;}
     int hp = 300;
     bool isDead = false;
+    sf::Sprite hero;
+    int knives = 0;
 };
 
 #endif //JOJO_RUN_HERO_H
