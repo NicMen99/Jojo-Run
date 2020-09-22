a7ba246050d1c7fb655bb8b8056b397c93272ce2
Niccolo <niccolo.menghini@stud.unifi.it>
2020-09-22 11:16:20 +0200

    Aggiunta la mappa

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <chrono>
+#include <SFML/Graphics.hpp>
 
+
+/** Qui è ancora tutto da rivedere per bene a conti fatti, è diverso da quello che abbiamo visto
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "JoJo Run");
+
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        window.clear();
+        //Qui va la roba da disegnare a schermo
+        window.display();
+    }
     return 0;
 }
