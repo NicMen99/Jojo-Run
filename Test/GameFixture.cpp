f9dbc13d4845cc0cc7917fdff2a49178b953872d
angiologiandonati <angiolo.giandonati@stud.unifi.it>
2020-10-04 20:44:37 +0200

    Aggiunti nuovi test

--- Test/GameFixture.cpp
+++ Test/GameFixture.cpp
@@ -12,6 +12,7 @@ protected:
     Hero hero;
     Factory factory;
     Block block;
+    FireWall wall;
     HamonEnemy en;
     FireEnemy en2;
     EmeraldEnemy en3;
@@ -39,5 +40,40 @@ TEST_F(GameTest, testDeletingBlocks) {
         blocks.erase(blocks.begin());
         ASSERT_TRUE(blockSize > game.getBlocksSize());
     }
+}
 
+TEST_F(GameTest, testDeletingFireWalls) {
+    int FWSize = game.getBlocksSize();
+    wall.setPosition(-wall.getGlobalBounds().width, wall.getPosition().y);
+    if (wall.getPosition().x + wall.getGlobalBounds().width < 0) {
+        firewalls.erase(firewalls.begin());
+        ASSERT_TRUE(FWSize > game.getFireWallsSize());
+    }
+}
+
+TEST_F(GameTest, testDeletingEmeraldEnemy) {
+    int enemySize = game.getEnemySize();
+    en3.setPosition(-en3.getGlobalBounds().width, en3.getPosition().y);
+    if (en3.getPosition().x + en3.getGlobalBounds().width < 0) {
+        enemies.erase(enemies.begin());
+        ASSERT_TRUE(enemySize > game.getEnemySize());
+    }
+}
+
+TEST_F(GameTest, testDeletingHamonEnemy) {
+    int enemySize = game.getEnemySize();
+    en.setPosition(-en.getGlobalBounds().width, en.getPosition().y);
+    if (en.getPosition().x + en.getGlobalBounds().width < 0) {
+        enemies.erase(enemies.begin());
+        ASSERT_TRUE(enemySize > game.getEnemySize());
+    }
+}
+
+TEST_F(GameTest, testDeletingFireEnemy) {
+    int enemySize = game.getEnemySize();
+    en2.setPosition(-en2.getGlobalBounds().width, en2.getPosition().y);
+    if (en2.getPosition().x + en2.getGlobalBounds().width < 0) {
+        enemies.erase(enemies.begin());
+        ASSERT_TRUE(enemySize > game.getEnemySize());
+    }
 }
